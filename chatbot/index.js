f77eca7f1d3373798d051b3ffdf6b703dec4ee21
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-30 23:45:28 +0530

    modified files

--- chatbot/index.js
+++ chatbot/index.js
@@ -1,6 +1,28 @@
-const { initializeDB } = require("../db.connect");
-require('dotenv').config();
-const mongoose = require("mongoose");
-const cors = require("cors");
-const express = require("express");
+// const { initializeDB } = require("../db.connect");
+// require('dotenv').config();
+// const mongoose = require("mongoose");
+import cors from 'cors';
+import express from 'express';
+import axios from 'axios';
+import ChatMessage from '../models/chatMessage.model.js';
+
+const router = express.Router();
+
+const MODEL = 'x-ai/grok-4.1-fast:free'
+
+router.post('/chat', async (req, res) => {
+    try{
+        const userId = req.user._id; 
+        const { message } = req.body; 
+
+        await ChatMessage.create({ userId, role: 'user', content: message});
+    } catch(error){
+        return res.status(502).json({error: "error"})
+    }
+})
+
+
+
+
+
                                                                     
---
+++ models/chatMessage.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const chatMessageSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, ref: "Usser", required: true //refferd to user schema.
+    },
+
+    role: {
+        type: String, 
+        enum: ['user', 'assistant'], required: true
+    },
+
+    content: {
+        type: String,
+        required: true
+    },
+},
+{timestamps: true}
+);
+
+export default mongoose.model('ChatMessage', chatMessageSchema);
